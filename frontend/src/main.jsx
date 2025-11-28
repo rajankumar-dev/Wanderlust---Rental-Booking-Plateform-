631db937283c001ee40522d0ed7074f4a597757d
rajankumar-dev <rk1620703@gmail.com>
2025-11-28 17:36:32 +0530

    complete main functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,24 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import AuthContext from './context/AuthContext.jsx'
+import AuthContext from './Context/AuthContext.jsx'
+import UserContext from './Context/UserContext.jsx'
+import ListingContext from './Context/ListingContext.jsx'
+import BookingContext from './Context/BookingContext.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AuthContext>
+    <ListingContext>
+    <UserContext>
+      <BookingContext>
     <App />
-  </AuthContext>
-  </BrowserRouter>,
+    </BookingContext>
+    </UserContext>
+    </ListingContext>
+    </AuthContext>
+  </BrowserRouter>
 )
