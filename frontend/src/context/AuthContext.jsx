4387c89342e174fa2423207f91a3d8ef5e4da122
rajankumar-dev <rk1620703@gmail.com>
2025-11-28 17:46:02 +0530

    complete context-backend functionality

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,14 +1,19 @@
-import React, { createContext } from 'react'
-export const authDataContext = createContext();
-
+import React, { createContext, useState } from 'react'
+export const authDataContext = createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:8000";
-    let value={serverUrl};
+    const serverUrl = "http://localhost:8000"
+
+    let [loading,setLoading]=useState(false)
+
+    let value={
+        serverUrl,
+        loading,setLoading
+    }
   return (
     <div>
-      <authDataContext.Provider value={value}>
+     <authDataContext.Provider value={value}>
         {children}
-      </authDataContext.Provider>
+     </authDataContext.Provider>
     </div>
   )
 }
