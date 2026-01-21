a77391d093138d7510f8f9e14139fe89b46f744e
rajankumar-dev <rk1620703@gmail.com>
2026-01-21 21:02:20 +0530

    Revert "complete context-backend functionality"
    This reverts commit 4387c89342e174fa2423207f91a3d8ef5e4da122.

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,19 +1,14 @@
-import React, { createContext, useState } from 'react'
-export const authDataContext = createContext()
+import React, { createContext } from 'react'
+export const authDataContext = createContext();
+
 function AuthContext({children}) {
-    const serverUrl = "http://localhost:8000"
-
-    let [loading,setLoading]=useState(false)
-
-    let value={
-        serverUrl,
-        loading,setLoading
-    }
+    let serverUrl = "http://localhost:8000";
+    let value={serverUrl};
   return (
     <div>
-     <authDataContext.Provider value={value}>
+      <authDataContext.Provider value={value}>
         {children}
-     </authDataContext.Provider>
+      </authDataContext.Provider>
     </div>
   )
 }
