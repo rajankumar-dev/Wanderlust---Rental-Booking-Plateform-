e0704021cce9e424d9603381123985e525cf805b
rajankumar-dev <rk1620703@gmail.com>
2026-01-22 23:01:23 +0530

    add context to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,20 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AuthContext from "./Context/AuthContext.jsx";
+import ListingContext from "./Context/ListingContext.jsx";
+import UserContext from "./Context/UserContext.jsx";
+import BookingContext from "./Context/BookingContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthContext>
-      <App />
+      <ListingContext>
+        <UserContext>
+          <BookingContext>
+            <App />
+          </BookingContext>
+        </UserContext>
+      </ListingContext>
     </AuthContext>
   </BrowserRouter>,
 );
