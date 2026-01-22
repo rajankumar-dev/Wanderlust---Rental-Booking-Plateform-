d94231458ac410dde3123da07dc1b8b6b6739dd2
rajankumar-dev <rk1620703@gmail.com>
2026-01-22 23:47:02 +0530

    create a unique homepage for our website

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useContext } from "react";
+import Nav from "../Component/Nav";
+
+import Card from "../Component/Card";
+import { listingDataContext } from "../Context/ListingContext";
 
 function Home() {
+  let { listingData, setListingData, newListData } =
+    useContext(listingDataContext);
+
   return (
-    <div className='text-[30px]'>
-      <h1>Home page</h1>
+    <div>
+      <Nav />
+      <div className="w-[100vw]  flex items-center justify-center gap-[25px] flex-wrap mt-[250px] md:mt-[180px]">
+        {newListData.map((list) => (
+          <Card
+            title={list.title}
+            landMark={list.landMark}
+            city={list.city}
+            image1={list.image1}
+            image2={list.image2}
+            image3={list.image3}
+            rent={list.rent}
+            id={list._id}
+            ratings={list.ratings}
+            isBooked={list.isBooked}
+            host={list.host}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
