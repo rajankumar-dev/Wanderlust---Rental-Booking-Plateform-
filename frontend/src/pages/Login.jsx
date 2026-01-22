d557b94ec0e54248acde52699a1c6ede372bc43c
rajankumar-dev <rk1620703@gmail.com>
2026-01-22 23:29:15 +0530

    fix some issues to completing login func..

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -40,7 +40,7 @@ function Login() {
     }
   };
   return (
-    <div className="w-[100vw] h-[100vh] flex item-center justify-center">
+    <div className="w-[100vw] h-[100vh] flex items-center justify-center relative">
       <div
         className="w-[50px] h-[50px] bg-[red] cursor-pointer absolute top-[10%] left-[20px] rounded-[50%] flex items-center justify-center"
         onClick={() => navigate("/")}
@@ -50,48 +50,58 @@ function Login() {
       <form
         action=""
         className="max-w-[900px] w-[90%] h-[600px] flex items-center justify-center flex-col md:items-start gap-[10px]"
+        onSubmit={handleLogin}
       >
-        <h1 className="text-[30px] text-[black]">Welcome to Wanderlust</h1>
+        <h1 className="text-[30px] text-[black]">Welcome to Airbnb</h1>
         <div className="w-[90%] flex items-start justify-start flex-col gap-[10px]">
           <label htmlFor="email" className="text-[20px]">
             Email
           </label>
           <input
-            type="email"
+            type="text"
             id="email"
-            className="w-[90%] h-[40px] border-[2px] border-[#555656] rounded-lg text-[18px] px-[15px]"
+            className="w-[90%] h-[40px] border-[2px] border-[#555656] rounded-lg text-[18px] px-[20px]"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
         </div>
-        <div className="w-[90%] flex items-start justify-start flex-col gap-[10px] relative">
+        <div className="w-[90%] flex items-start justify-start flex-col gap-[10px] relative  ">
           <label htmlFor="password" className="text-[20px]">
             Password
           </label>
           <input
             type={show ? "text" : "password"}
-            id="passsword"
-            className="w-[90%] h-[40px] border-[2px] border-[#555656] rounded-lg text-[18px] px-[15px]"
+            id="password"
+            className="w-[90%] h-[40px] border-[2px] border-[#555656] rounded-lg text-[18px] px-[20px] "
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
           />
           {!show && (
             <IoMdEye
-              className="w-[22px] h-[22px] absolute cursor-pointer right-[12%] bottom-[10px]"
+              className="w-[22px] h-[22px] absolute right-[12%] bottom-[10px] cursor-pointer"
               onClick={() => setShow((prev) => !prev)}
             />
           )}
           {show && (
             <IoMdEyeOff
-              className="w-[22px] h-[22px] absolute cursor-pointer  right-[12%] bottom-[10px]"
+              className="w-[22px] h-[22px] absolute right-[12%] bottom-[10px] cursor-pointer"
               onClick={() => setShow((prev) => !prev)}
             />
           )}
         </div>
-        <button className="px-[50px] py-[10px] bg-[red] text-[#fff] rounded-lg text-[18px] mt-[15px]">
-          Login
+        <button
+          className="px-[50px] py-[10px] bg-[red] text-[white] text-[18px] md:px-[100px] rounded-lg "
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Login"}
         </button>
         <p className="text-[18px]">
           Create new account{" "}
           <span
             className="text-[19px] text-[red] cursor-pointer"
-            onClick={() => navigate("/signup")}
+            onClick={() => navigate("/SignUP")}
           >
             SignUp
           </span>
