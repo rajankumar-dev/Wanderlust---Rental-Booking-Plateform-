02917a86e98394932c39915e4cc439bf3db4287e
rajankumar-dev <rk1620703@gmail.com>
2026-01-22 22:29:07 +0530

    add cors package to backend and fix some issues

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,22 @@ import connectDb from "./config/db.js";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
+import cors from "cors";
 
 let port = process.env.PORT || 8000;
 let app = express();
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
-    connectDb();
-    console.log("server started");
-})
-
+  connectDb();
+  console.log("server started");
+});
