d2391544e5371edba41303f6becdc0e53ea27997
rajankumar-dev <rk1620703@gmail.com>
2026-01-24 10:12:51 +0530

    fix some production error and add cors url

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://wanderlust-rental-booking-plateform-ppy9.onrender.com",
+    ],
     credentials: true,
   }),
 );
