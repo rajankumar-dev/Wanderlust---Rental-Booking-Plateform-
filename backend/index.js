86084cab625ea1b293cdf81fcc6ab22d07c3b920
rajankumar-dev <rk1620703@gmail.com>
2026-01-24 16:55:54 +0530

    Revert "Update CORS origin to production URL"
    This reverts commit d8b400ee716fde2b09510aa0471c7b2ea6d85b0f.

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      "https://wanderlust-rental-booking-plateform-ppy9.onrender.com",
-      "http://localhost:5173",
-    ],
+    origin: "http://localhost:5173",
     credentials: true,
   }),
 );
