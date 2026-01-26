0fb0d5435af8e09d7115f88d909f1520b3cd2e71
rajankumar-dev <rk1620703@gmail.com>
2026-01-26 11:29:17 +0530

    refactor Footer component layout and enhance link structure

--- frontend/src/Component/Footer.jsx
+++ frontend/src/Component/Footer.jsx
@@ -1,119 +1,247 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaAngleDown } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="w-[100%] h-[40vh] bg-gray-200 ">
-      <div className="pt-10 ml-[4rem]">
-        <h1 className="pt-1 ml-1 text-2xl font-medium text-gray-800">
-          Inspiration for future getaways
-        </h1>
-        <div className="flex justify-evenly flex-wrap mt-5 ml-[-13rem]">
-          <div className="flex flex-col gap-5 mb-[3rem]">
-            <ul>
-              <li className=" mb-[2rem]">
-                <Link to="/" className="text-gray-900 hover:underline">
-                  Galveston
-                </Link>
-              </li>
-              <li className=" mb-[2rem]">
-                <Link to="/" className="text-gray-900 hover:underline">
-                  Athens
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" className="text-gray-900 hover:underline">
-                  Gulf Shores
-                </Link>
-              </li>
-            </ul>
-          </div>
+    <>
+      <hr />
+      <div className="w-[100%] h-[40vh] bg-gray-50 ">
+        <div className="pt-7 ml-[5rem] md:ml-[1rem]]">
+          <h1 className="pt-1 text-2xl  font-medium text-gray-800">
+            Inspiration for future getaways
+          </h1>
+          <div className=" flex justify-evenly flex-col flex-wrap mt-5 sm:flex-row sm:flex-wrap md:flex-row md:ml-[-8rem] ">
+            <div className="flex flex-col gap-5 mb-[3rem]">
+              <ul>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Galveston
+                  </Link>{" "}
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Monthly Rentals
+                  </Link>
+                </li>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Athens
+                  </Link>
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Apartment rentals
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/about"
+                    className="text-gray-800 hover:text-gray-900"
+                  >
+                    Gulf Shores
+                  </Link>
+                  <br />
+                  <Link
+                    to="/about"
+                    className="text-gray-500 hover:text-gray-700"
+                  >
+                    Holiday rentals
+                  </Link>
+                </li>
+              </ul>
+            </div>
 
-          <div className="flex flex-col gap-5 mb-[3rem]">
-            <ul>
-              <li className=" mb-[2rem]">
-                <Link to="/" className="text-gray-900 hover:underline">
-                  Barcelona
-                </Link>
-              </li>
-              <li className=" mb-[2rem]">
-                <Link to="/" className="text-gray-900 hover:underline">
-                  Charlotte
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" className="text-gray-900 hover:underline">
-                  Domingo
-                </Link>
-              </li>
-            </ul>
-          </div>
+            <div className="flex flex-col gap-5 mb-[3rem]">
+              <ul>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Barcelona
+                  </Link>{" "}
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Villa rentals
+                  </Link>
+                </li>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Charlotte
+                  </Link>
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Apartment rentals
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/about"
+                    className="text-gray-800 hover:text-gray-900"
+                  >
+                    Santo
+                  </Link>
+                  <br />
+                  <Link
+                    to="/about"
+                    className="text-gray-500 hover:text-gray-700"
+                  >
+                    Flat rentals
+                  </Link>
+                </li>
+              </ul>
+            </div>
 
-          <div className="flex flex-col gap-5 mb-[3rem]">
-            <ul>
-              <li className=" mb-[2rem]">
-                <Link to="/" className="text-gray-900 hover:underline">
-                  Oahu
-                </Link>
-              </li>
-              <li className=" mb-[2rem]">
-                <Link to="/" className="text-gray-900 hover:underline">
-                  Montreal
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" className="text-gray-900 hover:underline">
-                  Madrid
-                </Link>
-              </li>
-            </ul>
-          </div>
+            <div className="flex flex-col gap-5 mb-[3rem]">
+              <ul>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Oahu
+                  </Link>{" "}
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Apartment rentals
+                  </Link>
+                </li>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Montreal
+                  </Link>
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Monthly Rentals
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/about"
+                    className="text-gray-800 hover:text-gray-900"
+                  >
+                    Madrid
+                  </Link>
+                  <br />
+                  <Link
+                    to="/about"
+                    className="text-gray-500 hover:text-gray-700"
+                  >
+                    House rentals
+                  </Link>
+                </li>
+              </ul>
+            </div>
 
-          <div className="flex flex-col gap-5 mb-[3rem]">
-            <ul>
-              <li className=" mb-[2rem]">
-                <Link to="/" className="text-gray-900 hover:underline">
-                  Savannah
-                </Link>
-              </li>
-              <li className=" mb-[2rem]">
-                <Link to="/" className="text-gray-900 hover:underline">
-                  Philadelphia
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" className="text-gray-900 hover:underline">
-                  Cleveland
-                </Link>
-              </li>
-            </ul>
-          </div>
+            <div className="flex flex-col gap-5 mb-[3rem]">
+              <ul>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Savannah
+                  </Link>{" "}
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Apartment rentals
+                  </Link>
+                </li>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Philadelphia
+                  </Link>
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Apartment rentals
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/about"
+                    className="text-gray-800 hover:text-gray-900"
+                  >
+                    Manhattan
+                  </Link>
+                  <br />
+                  <Link
+                    to="/about"
+                    className="text-gray-500 hover:text-gray-700"
+                  >
+                    Apartment rentals
+                  </Link>
+                </li>
+              </ul>
+            </div>
 
-          <div className="flex flex-col gap-5 mb-[3rem]">
-            <ul>
-              <li className=" mb-[2rem]">
-                <Link to="/" className="text-gray-900 hover:underline">
-                  Milan
-                </Link>
-              </li>
-              <li className=" mb-[2rem]">
-                <Link to="/" className="text-gray-900 hover:underline">
-                  Kansas
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" className="text-gray-900 hover:underline">
-                  Daytona
-                </Link>
-              </li>
-            </ul>
+            <div className="flex flex-col gap-5 mb-[3rem]">
+              <ul>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Orange Beach
+                  </Link>{" "}
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Apartment rentals
+                  </Link>
+                </li>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Milan
+                  </Link>
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Villa rentals
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/about"
+                    className="text-gray-800 hover:text-gray-900"
+                  >
+                    Kansas City
+                  </Link>
+                  <br />
+                  <Link
+                    to="/about"
+                    className="text-gray-500 hover:text-gray-700"
+                  >
+                    Apartment rentals
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="flex flex-col gap-5 mb-[3rem]">
+              <ul>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Daytona Beach
+                  </Link>{" "}
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Apartment rentals
+                  </Link>
+                </li>
+                <li className=" mb-[2rem]">
+                  <Link to="/" className="text-gray-800 hover:text-gray-900">
+                    Philadelphia
+                  </Link>
+                  <br />
+                  <Link to="/" className="text-gray-500 hover:text-gray-700">
+                    Apartment rentals
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/about"
+                    className="text-gray-800 hover:text-gray-900 hover:underline"
+                  >
+                    Show more
+                    <span>
+                      <FaAngleDown className="inline " />
+                    </span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
+        <div className="mt-10 text-gray-600 ml-[3rem] ">
+          © 2025 Wanderlust Rental Booking Platform. All rights reserved.
+        </div>
       </div>
-      <div className="mt-10 text-gray-600 ml-[3rem] ">
-        © 2025 Wanderlust Rental Booking Platform. All rights reserved.
-      </div>
-    </div>
+    </>
   );
 };
 
