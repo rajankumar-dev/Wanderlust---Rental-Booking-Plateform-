0b13cd78d454b46719d52f6d534168df9086413c
rajankumar-dev <rk1620703@gmail.com>
2026-09-02 21:12:33 +0530

    fix: adjust padding and gap in Booked component layout

--- frontend/src/pages/Booked.jsx
+++ frontend/src/pages/Booked.jsx
@@ -46,8 +46,8 @@ function Booked() {
 
   return (
     <div className="w-[100vw] min-h-[100vh] flex items-center justify-center gap-[10px] bg-slate-200 flex-col">
-      <div className="w-[95%] max-w-[500px] h-[400px] bg-[white] flex items-center justify-center border-[1px] border-[#b5b5b5] flex-col gap-[20px] p-[20px] md:w-[80%] rounded-lg">
-        <div className="w-[100%] h-[50%] text-[20px] flex items-center justify-center flex-col gap-[20px] font-semibold">
+      <div className="w-[95%] max-w-[500px] h-[400px] bg-[white] flex items-center justify-center border-[1px] border-[#b5b5b5] flex-col gap-[20px] p-[19px] md:w-[80%] rounded-lg">
+        <div className="w-[100%] h-[50%] text-[20px] flex items-center justify-center flex-col gap-[19px] font-semibold">
           <GiConfirmed className="w-[100px] h-[100px] text-[green]" />
           Booking Confirmed
         </div>
